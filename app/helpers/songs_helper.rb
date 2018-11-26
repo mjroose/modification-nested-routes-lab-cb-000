d4b041a2673b2f1d0cd65fec4db27c066dacff6e
Michael Roose <mjroose@gmail.com>
2018-11-26 18:38:58 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,8 @@
 module SongsHelper
-
+  def artist_select(artist)
+    if artist
+      content_tag :p, artist.name
+    else
+      select_tag @artists, options_from_collection_for_select(@artists, :id, :name)
+  end
 end
