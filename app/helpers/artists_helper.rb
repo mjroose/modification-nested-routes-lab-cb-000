bf5236a29064982a5943957360bce3cb125becb2
Michael Roose <mjroose@gmail.com>
2018-11-26 18:48:38 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,13 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(artist, artists)
+    if artist
+      hidden_field_tag :artist_id
+      content_tag :p, artist.name
+    else
+      select_tag :artist, options_from_collection_for_select(@artists, :id, :name)
+    end
+  end
 end
